7e6b5d393109d30a2d1ca1c08fea9265b3cb964d
xetra11 <xetra11@outlook.com>
2020-11-23 22:28:40 +0100

    feat(character): add character model first iteration

---
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/Character.kt
@@ -0,0 +1,27 @@
+package com.github.xetra11.ck3workbench.module.character
+
+/**
+ * Representation of a CK3 script history/character
+ *
+ * @author Patrick C. Höfer
+ */
+
+data class Character(
+    val name: String,
+    val dna: String,
+    val dynasty: String,
+    val religion: String,
+    val culture: String,
+    val skills: Map<Skill, String>,
+    val traits: List<String>,
+    val birth: String,
+    val death: String
+) {
+    enum class Skill {
+        MARTIAL,
+        STEWARDSHIP,
+        DIPLOMACY,
+        INTRIGUE,
+        LEARNING
+    }
+}
--- src/main/kotlin/com/github/xetra11/ck3workbench/module/character/ui/NewCharacterForm.kt
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/ui/NewCharacterForm.kt
@@ -1,5 +1,7 @@
-package com.github.xetra11.ck3workbench.module.character.ui
+package com.github.xetra11.ck3workbench.module.character
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,7 +10,10 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 /**
  * @author Patrick C. Höfer
@@ -16,12 +21,12 @@ import androidx.compose.ui.text.input.TextFieldValue
 
 @Composable
 fun NewCharacterForm() {
-    Box(){
+    Box(Modifier.border(1.dp, Color.Black)){
         Column(){
             Row(){
-                Text("Name")
                 val characterName = remember { mutableStateOf(TextFieldValue()) }
                 TextField(
+                    label =  { Text("Name") },
                     value = characterName.value,
                     onValueChange = { characterName.value = it },
                     placeholder = { Text("Enter name of character") }
