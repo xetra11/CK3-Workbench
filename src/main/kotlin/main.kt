7ddc90acb139fc1045fb89059a61a119f33a675f
xetra11 <xetra11@outlook.com>
2020-11-26 15:13:00 +0100

    feat(gradle): add icon

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,9 +1,24 @@
+import androidx.compose.desktop.AppManager
+import androidx.compose.desktop.AppWindow
 import androidx.compose.desktop.Window
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.lightColors
+import androidx.compose.ui.window.Menu
+import androidx.compose.ui.window.MenuBar
+import androidx.compose.ui.window.MenuItem
 import com.github.xetra11.ck3workbench.module.character.view.CharacterModuleView
 
-fun main() = Window(title = "CK3 Mod Workbench") {
+fun main() = Window(
+    title = "CK3 Mod Workbench",
+    menuBar = MenuBar(
+        Menu("File", MenuItem("Exit", onClick = { AppManager.exit() })),
+        Menu(
+            "History",
+            MenuItem("Characters", onClick = {}),
+            MenuItem("Dynasties", onClick = {})
+        ),
+    )
+) {
     MaterialTheme(lightColors()) {
         CharacterModuleView()
     }
