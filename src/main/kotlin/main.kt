199f4423cded87fc4a9dabc1f87145a2e7dd810e
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-11-29 17:48:25 +0100

    feat(character): add script validation alert

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,40 +1,48 @@
 import androidx.compose.desktop.AppManager
 import androidx.compose.desktop.AppWindow
-import androidx.compose.desktop.AppWindowAmbient
-import androidx.compose.desktop.Window
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Shapes
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.AmbientWindowManager
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Menu
 import androidx.compose.ui.window.MenuBar
 import androidx.compose.ui.window.MenuItem
+import com.github.xetra11.ck3workbench.ScriptValidator
+import com.github.xetra11.ck3workbench.ScriptValidator.ValidationError
 import com.github.xetra11.ck3workbench.module.character.Character
 import com.github.xetra11.ck3workbench.module.character.CharacterTemplate
 import com.github.xetra11.ck3workbench.module.character.importer.CharacterScriptReader
+import com.github.xetra11.ck3workbench.module.character.importer.CharacterScriptValidator
 import com.github.xetra11.ck3workbench.module.character.view.CharacterModuleView
+import com.github.xetra11.ck3workbench.styles.WorkbenchButtons.BasicButton
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.awt.FileDialog
 import java.io.File
 import javax.swing.SwingUtilities.invokeLater
 
 fun main() = invokeLater {
+    val LOG: Logger = LoggerFactory.getLogger("Main")
     lateinit var window: AppWindow
+    val hasAlert = mutableStateOf(false)
+    val validationErrors = mutableStateListOf<ValidationError>()
 
     val characterState: SnapshotStateList<Character> =
         mutableStateListOf(
             CharacterTemplate.DEFAULT_CHARACTER,
             CharacterTemplate.DEFAULT_CHARACTER,
             CharacterTemplate.DEFAULT_CHARACTER
-    )
+        )
 
     window = AppWindow(
         title = "CK3 Mod Workbench",
@@ -50,9 +58,21 @@ fun main() = invokeLater {
                     fileDialog.isVisible = true
                     file.value = File(fileDialog.directory + fileDialog.file)
 
-                    val characterScriptReader = CharacterScriptReader()
-                    val character = characterScriptReader.readCharacterScript(file.value.absoluteFile)
-                    character?.let { characterState.add(it) }
+                    val characterScriptValidator = CharacterScriptValidator()
+                    val validation = characterScriptValidator.validate(file.value.readText())
+                    if (validation.hasErrors) {
+                        LOG.error("Script to be imported has errors")
+                        LOG.error("Errors found:")
+                        validation.errors.forEach {
+                            LOG.error(it.reason)
+                        }
+                        validationErrors.addAll(validation.errors)
+                        hasAlert.value = true
+                    } else {
+                        val characterScriptReader = CharacterScriptReader()
+                        val character = characterScriptReader.readCharacterScript(file.value.absoluteFile)
+                        character?.let { characterState.add(it) }
+                    }
                 }),
                 MenuItem("Dynasties", onClick = {})
             ),
@@ -64,9 +84,37 @@ fun main() = invokeLater {
             colors = workbenchLightColors(),
             shapes = workBenchShapes()
         ) {
+            if (hasAlert.value) {
+                CharacterValidationErrorAlert(hasAlert, validationErrors)
+            }
             CharacterModuleView(characterState)
         }
     }
+}
+
+@Composable
+private fun CharacterValidationErrorAlert(
+    alertState: MutableState<Boolean>,
+    validationErrors: SnapshotStateList<ValidationError>
+) {
+    AlertDialog(
+        title = { Text("Validation Error") },
+        text = {
+            Row { Text("Following validation reasons occured: ") }
+            validationErrors.forEach {
+                Row { Text("- $it") }
+            }
+        },
+        onDismissRequest = {},
+        buttons = {
+            BasicButton(
+                onClick = { alertState.value = false }
+            ) {
+                Text("Ok")
+            }
+        }
+    )
+
 }
 
 fun workbenchLightColors(
