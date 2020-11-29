c810d44852ddc90e02089d0afc5e8f44cbeee721
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-11-29 17:10:52 +0100

    feat(character): add scrollbar

--- src/main/kotlin/com/github/xetra11/ck3workbench/module/character/ui/CharacterTable.kt
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/ui/CharacterTable.kt
@@ -1,13 +1,25 @@
 package com.github.xetra11.ck3workbench.module.character.ui
 
+import androidx.compose.foundation.HorizontalScrollbar
+import androidx.compose.foundation.ScrollableRow
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.github.xetra11.ck3workbench.module.character.Character
 
 /**
@@ -20,21 +32,23 @@ fun CharacterTable(
     modifier: Modifier = Modifier,
     characterState: SnapshotStateList<Character>
 ) {
-    TableHeader(
-        modifier.then(Modifier.background(Color.LightGray)),
-        items = listOf(
-            "name",
-            "dna",
-            "dynasty",
-            "culture",
-            "religion",
-            "birth",
-            "death"
-        )
-    )
+    val scrollState = rememberScrollState(0f)
+
+    val headerModifier = modifier.background(Color.LightGray)
+    ScrollableRow(scrollState = scrollState) {
+        Box(headerModifier) { Text("name") }
+        Box(headerModifier) { Text("dna") }
+        Box(headerModifier) { Text("dynasty") }
+        Box(headerModifier) { Text("culture") }
+        Box(headerModifier) { Text("religion") }
+        Box(headerModifier) { Text("birth") }
+        Box(headerModifier) { Text("death") }
+    }
 
     characterState.map {
-        Row {
+        ScrollableRow(
+            scrollState = scrollState
+        ) {
             Box(modifier) { Text(it.name) }
             Box(modifier) { Text(it.dna) }
             Box(modifier) { Text(it.dynasty) }
@@ -44,4 +58,8 @@ fun CharacterTable(
             Box(modifier) { Text(it.death) }
         }
     }
+    HorizontalScrollbar(
+        modifier = Modifier.fillMaxWidth().height(15.dp),
+        adapter = rememberScrollbarAdapter(scrollState)
+    )
 }
--- src/main/kotlin/com/github/xetra11/ck3workbench/module/character/view/CharacterModuleView.kt
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/view/CharacterModuleView.kt
@@ -3,6 +3,7 @@ package com.github.xetra11.ck3workbench.module.character.view
 import androidx.compose.foundation.ScrollableRow
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -12,11 +13,9 @@ import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.github.xetra11.ck3workbench.module.character.AddCharacterDialog
 import com.github.xetra11.ck3workbench.module.character.Character
 import com.github.xetra11.ck3workbench.module.character.CharacterTemplate
 import com.github.xetra11.ck3workbench.module.character.ui.CharacterList
-import com.github.xetra11.ck3workbench.module.character.window.CharacterScriptImportControls
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -30,7 +29,7 @@ fun CharacterModuleView(
             .border(2.dp, Color.Black)
             .fillMaxWidth()
     ) {
-        ScrollableRow {
+        Row {
             CharacterList(characterState)
         }
     }
