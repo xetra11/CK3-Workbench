370268e3ef5daafa6b405b147c6edd8d911e6b88
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-11-30 15:16:08 +0100

    feat(tokenizer): finish init tokenizer logic

--- src/main/kotlin/com/github/xetra11/ck3workbench/module/character/importer/ScriptTokenizer.kt
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/importer/ScriptTokenizer.kt
@@ -1,5 +1,7 @@
 package com.github.xetra11.ck3workbench.module.character.importer
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.io.File
 
 /*
@@ -20,13 +22,39 @@ class ScriptTokenizer {
     }
 
     fun tokenize(scriptContent: String): List<Token> {
-        return scriptContent
+        val preparedToken =  scriptContent
             .trim()
             .split(" ")
             .filterNot { it.isBlank() }
             .map { it.replace("\n", "") }
             .map { segment -> segment to resolveSymbols(segment)}
             .map { Token(it.first, it.second)}
+
+        return resolveIdentifier(preparedToken.toMutableList())
+    }
+
+    private fun resolveIdentifier(preparedToken: MutableList<Token>): List<Token> {
+        val indexOfAssignmentOperation = preparedToken.indexOfFirst { it.type == TokenType.ASSIGNMENT }
+
+        if (indexOfAssignmentOperation <= 0) {
+            LOG.error("Assignment operation can not exist without left side value")
+        }
+        if (indexOfAssignmentOperation >= preparedToken.size + 1) {
+            LOG.error("Assignment operation can not exist without right side value")
+        }
+
+        val leftAssignmentToken = preparedToken[indexOfAssignmentOperation - 1]
+        val rightAssignmentToken = preparedToken[indexOfAssignmentOperation + 1]
+
+        if (leftAssignmentToken.type == TokenType.UNTYPED) {
+            preparedToken[indexOfAssignmentOperation - 1] = Token(leftAssignmentToken.value, TokenType.IDENTIFIER)
+        }
+
+        if (rightAssignmentToken.type == TokenType.UNTYPED) {
+            preparedToken[indexOfAssignmentOperation + 1] = Token(rightAssignmentToken.value, TokenType.VALUE)
+        }
+
+        return preparedToken
     }
 
     private fun resolveSymbols(string: String): TokenType {
@@ -42,4 +70,8 @@ class ScriptTokenizer {
         val value: String,
         val type: TokenType
     )
+
+    companion object {
+        private val LOG: Logger = LoggerFactory.getLogger(ScriptTokenizer::class.java)
+    }
 }
--- src/test/kotlin/com/github/xetra11/ck3workbench/module/character/importer/ScriptTokenizerTest.kt
+++ src/test/kotlin/com/github/xetra11/ck3workbench/module/character/importer/ScriptTokenizerTest.kt
@@ -4,25 +4,56 @@ import com.github.xetra11.ck3workbench.module.character.importer.ScriptTokenizer
 import com.github.xetra11.ck3workbench.module.character.importer.ScriptTokenizer.TokenType
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.io.File
 
 internal class ScriptTokenizerTest {
     private val scriptTokenizer: ScriptTokenizer = ScriptTokenizer()
 
     @Test
-    fun `should return a section representing a character script definition`(){
+    fun `should return a simple tokenization for empty character script definition`(){
         val actual = scriptTokenizer.tokenize(TEST_SCRIPT_1)
 
-        assertThat(actual).element(0).isEqualTo(Token("thorak", TokenType.UNTYPED))
-        assertThat(actual).element(1).isEqualTo(Token("=", TokenType.ASSIGNMENT))
-        assertThat(actual).element(2).isEqualTo(Token("{", TokenType.L_BRACE))
-        assertThat(actual).element(3).isEqualTo(Token("}", TokenType.R_BRACE))
+        assertThat(actual).containsExactly(
+            Token("thorak", TokenType.IDENTIFIER),
+            Token("=", TokenType.ASSIGNMENT),
+            Token("{", TokenType.L_BRACE),
+            Token("}", TokenType.R_BRACE)
+        )
     }
+
+    fun `should fail if script has null value left of assignment`(){
+        val actual = scriptTokenizer.tokenize(NO_LEFT_VALUE)
+
+        assertThat(actual).containsExactly(
+            Token("thorak", TokenType.IDENTIFIER),
+            Token("=", TokenType.ASSIGNMENT),
+            Token("{", TokenType.L_BRACE),
+            Token("}", TokenType.R_BRACE)
+        )
+    }
+
+    fun `should fail if script has null value right of assignment`(){
+        val actual = scriptTokenizer.tokenize(NO_RIGHT_VALUE)
+
+        assertThat(actual).containsExactly(
+            Token("thorak", TokenType.IDENTIFIER),
+            Token("=", TokenType.ASSIGNMENT),
+            Token("{", TokenType.L_BRACE),
+            Token("}", TokenType.R_BRACE)
+        )
+    }
+
+
 
     companion object {
         const val TEST_SCRIPT_1 = """
-            thorak =  {
+            thorak =  { }
+        """
+        const val NO_LEFT_VALUE = """
+            =  {
             }
+        """
+        const val NO_RIGHT_VALUE = """
+            thorak =
         """
     }
 }
