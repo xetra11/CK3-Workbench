5a72504c3493072878e17899fa54521d94174ee0
xetra11 <xetra11@outlook.com>
2020-12-08 20:20:41 +0100

    feat(validator): add validator wip

---
+++ src/main/kotlin/com/github/xetra11/ck3workbench/module/character/importer/tokenizer/GrammarValidator.kt
@@ -0,0 +1,12 @@
+package com.github.xetra11.ck3workbench.module.character.importer.tokenizer
+
+/**
+ * Validates given scripts against grammar syntax definitions
+ *
+ * @author Patrick C. Höfer
+ */
+class GrammarValidator {
+
+
+
+}
