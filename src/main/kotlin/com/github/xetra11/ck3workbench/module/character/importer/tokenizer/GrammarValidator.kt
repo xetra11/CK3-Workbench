7e7f222b4001838eaac8a82df61888cd048b5647
xetra11 <xetra11@outlook.com>
2020-12-09 00:15:01 +0100

    refactor(validator): remove validator try

--- src/main/kotlin/com/github/xetra11/ck3workbench/module/character/importer/tokenizer/GrammarValidator.kt
+++
@@ -1,8 +0,0 @@
-package com.github.xetra11.ck3workbench.module.character.importer.tokenizer
-
-/**
- * Validates given scripts against grammar syntax definitions
- *
- * @author Patrick C. Höfer
- */
-class GrammarValidator
