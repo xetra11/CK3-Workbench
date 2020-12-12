beb705edd98a2c11125bb508fdcc758da7b44983
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-12-12 13:28:37 +0100

    feat(detekt): add detekt static code analysis

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,19 @@
+import io.gitlab.arturbosch.detekt.Detekt
 import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        jcenter()
+    }
+}
 
 plugins {
     kotlin("jvm") version "1.4.21"
     id("org.jetbrains.compose") version "0.2.0-build128"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
+    id("io.gitlab.arturbosch.detekt") version "1.15.0-RC1"
 }
 
 group = "com.github.xetra11"
@@ -48,5 +56,25 @@ compose.desktop {
                 dirChooser = true
             }
         }
+    }
+}
+
+detekt {
+    failFast = true // fail build on any finding
+    buildUponDefaultConfig = true // preconfigure defaults
+    config = files("$projectDir/detekt/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+    baseline = file("$projectDir/detekt/baseline.xml") // a way of suppressing issues before introducing detekt
+
+    reports {
+        html.enabled = true // observe findings in your browser with structure and code snippets
+        xml.enabled = true // checkstyle like format mainly for integrations like Jenkins
+        txt.enabled = true // similar to the console output, contains issue signature to manually edit baseline files
+    }
+}
+
+tasks {
+    withType<Detekt> {
+        // Target version of the generated JVM bytecode. It is used for type resolution.
+        this.jvmTarget = "1.8"
     }
 }
