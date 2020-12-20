d15c42591296b70d3b49767365d074a4396b2c44
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-12-20 14:49:32 +0100

    chore(scripts): priotize script as release script

--- release.sh
+++ release.sh
@@ -1,2 +1,18 @@
+echo "[Bump Push]"
+git pull
+echo "resolve git tags"
+last_version=`git describe --tags --abbrev=0`
+echo "last version: $last_version"
+new_version=`bump patch --dry-run --allow-dirty`
+echo "new version (bump): $new_version"
+echo "update README.md with new versions"
+sed -i "s/$last_version/$new_version/g" README.md
+
+git add *README.md
+git commit -m "chore(version): update version"
+
+git tag $new_version
+git push && git push --tag
+
 #npx github-release-notes@0.17.1 release --token=$(echo $GREN_GITHUB_TOKEN) -P
-npx github-release-notes@0.17.1 changelog --token=$(echo $GREN_GITHUB_TOKEN)
+#npx github-release-notes@0.17.1 changelog --token=$(echo $GREN_GITHUB_TOKEN)
