a84e43fb27ba2833fe466eb9b7fd6c0e74667dab
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-12-20 19:04:37 +0100

    chore(scripts): add echo for git status

--- release.sh
+++ release.sh
@@ -14,6 +14,7 @@ npx github-release-notes@0.17.1 changelog --token=$(echo $GITHUB_TOKEN) --overri
 git add *README.md
 git add *CHANGELOG.md
 git add *bug_report.md
+echo `git status`
 git commit -m "chore(version): update version"
 git tag $new_version -f
 
