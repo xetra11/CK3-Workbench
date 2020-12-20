4f35a6b133af00c27151a41f2700eb4e7fa2880b
Patrick Hoefer <patrick.hoefer@promaterial.com>
2020-12-20 13:12:28 +0100

    github(release): try adding release note command with gren

--- release.sh
+++ release.sh
@@ -1,2 +1,2 @@
-npx github-release-notes@0.17.1 release --token=$(echo $GREN_GITHUB_TOKEN) -P
+#npx github-release-notes@0.17.1 release --token=$(echo $GREN_GITHUB_TOKEN) -P
 npx github-release-notes@0.17.1 changelog --token=$(echo $GREN_GITHUB_TOKEN)
